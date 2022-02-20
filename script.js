92c06f673ad2ca6b70b9bf93fc3a033be555e425
Emerge <egreengirl1@gmail.com>
2022-02-19 17:59:01 -0700

    moved files

---
+++ script.js
@@ -0,0 +1,11 @@
+// // create DOWNLOAD RESUME button and append to the RESUME section
+// let resumeTitleEl = $('#resume');
+// let downloadButtonEl = $('<button>');
+// downloadButtonEl.attr
+// downloadButtonEl.text('Download PDF');
+// // downloadButtonEl.addClass('') 
+// // add a bunch of TAILWIND.CSS CLASS TO STYLE THE BUTTON 
+// resumeTitleEl.append(downloadButtonEl);
+
+//EVENT LISTENER FUNCTION: Download pdf of resume to user's machine
+
